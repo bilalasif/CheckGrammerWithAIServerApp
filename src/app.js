0bb37b38e0e5ba475c1c27d08b327ef61d76e11f
Bilal Asif <bilalasif435@hotmail.com>
2025-03-18 17:01:23 +0500

    deploy,ent config in nev needed to be setup

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ dotenv.config();
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
